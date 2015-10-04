ffbc18f2729776d27252c6bdb98adfdfd3c93e38
Alexander Ivanov <alehander42@gmail.com>
2015-10-04 16:05:48 +0300

    Fix formatting

--- motherbase.py
+++ motherbase.py
@@ -10,22 +10,23 @@ import cortext.visualizer
 
 app = Flask(__name__)
 
+
 @app.route('/visualize_words/')
 def visualize_words():
-	url = request.args.get('url', '')
-	data = readabillity.load(url)
-	raw = BeautifulSoup(data['content']).get_text()
-	text = cortext.text.Text(raw).parse()
-	v = cortext.visualizer.Visualizer(text, API_CONFIG)
-	words = [[to_data(obj) for obj in s] for s in v.visualize_words()]
-	return flask.jsonify(**{'words': words, 'content': data['content'], 'title': data['title']})
+    url = request.args.get('url', '')
+    data = readabillity.load(url)
+    raw = BeautifulSoup(data['content']).get_text()
+    text = cortext.text.Text(raw).parse()
+    v = cortext.visualizer.Visualizer(text, API_CONFIG)
+    words = [[to_data(obj) for obj in s] for s in v.visualize_words()]
+    return flask.jsonify(**{'words': words, 'content': data['content'], 'title': data['title']})
+
 
 def to_data(obj):
-	out = {'word_type': type(obj).__name__}
-	out.update(obj.__dict__)
-	return out
+    out = {'word_type': type(obj).__name__}
+    out.update(obj.__dict__)
+    return out
 
 if __name__ == '__main__':
-    #app.run(debug=True)
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
-
